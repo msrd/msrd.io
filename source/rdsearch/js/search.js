ce47a0283c8f20d906c2e898128af29edf636b00
MattTalbot <matttalbot306@gmail.com>
2013-05-30 12:09:45 -0700

    added filter to show current number of displayed items

--- source/rdsearch/js/search.js
+++ source/rdsearch/js/search.js
@@ -1,7 +1,13 @@
-function listCtrl($scope, $http){
+var app = angular.module('msrdio', [])
 
-    $http.get('test.json').success(function(callbackData)
+    app.controller('listCtrl', function($scope, $http, $filter)
     {
-        $scope.directors = callbackData;
+        $http.get('test.json').success(function(callbackData)
+        {
+            $scope.directors = callbackData;
+            
+            $scope.$watch("searchText", function(query){
+                $scope.filteredData = $filter("filter")($scope.directors, query);
+            });
+        });
     });
-}
