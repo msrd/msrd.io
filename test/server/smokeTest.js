8d008ce717806a518fefbff7dbc42411ca7301e8
Jason Jarrett <jason@elegantcode.com>
2013-06-03 13:01:43 -0700

    checking in a failing tests to give travis-ci a quick check

--- test/server/smokeTest.js
+++ test/server/smokeTest.js
@@ -1,4 +1,7 @@
 describe('Smoke Test', function(){
     it('should pass', function() {
     });
+    it('should fail', function() {
+        throw "this is a sample test failure - testing the bits";
+    });
 });
