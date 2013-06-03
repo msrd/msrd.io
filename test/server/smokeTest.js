01f0ccc6a75241ac9321592b3fbf61c75f1b621e
Jason Jarrett <jason@elegantcode.com>
2013-06-03 13:03:51 -0700

    removing failing test - yay for C.I. checks

--- test/server/smokeTest.js
+++ test/server/smokeTest.js
@@ -1,7 +1,4 @@
 describe('Smoke Test', function(){
     it('should pass', function() {
     });
-    it('should fail', function() {
-        throw "this is a sample test failure - testing the bits";
-    });
 });
