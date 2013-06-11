cf7abaccd32941e21539a40db061d5b4d29ecb5d
MattTalbot <matttalbot306@gmail.com>
2013-06-11 09:59:16 -0700

    Squished bugs and cleaned up CSS

---
+++ source/js/controller.js
@@ -0,0 +1,42 @@
+var app = angular.module('msrd', [])
+
+app.factory('RdList', ['$http', function ($http) {
+    return{
+        get: function(callback){
+            $http.get('../rdlist.json').success(function(item){
+                item.forEach(function (data){
+                    data.FullName = data.First + ' ' + data.Last;
+                    if(data.GravatarHash){
+                        data.Avatar = 'http://www.gravatar.com/avatar/' + data.GravatarHash + '?s=200';
+                    }
+                    else{
+                        data.Avatar = 'http://placekitten.com/g/200/200';
+                    }
+                });
+                callback(item);
+            });
+        }
+    };
+}]);
+
+app.controller('searchCtrl', function($scope, RdList) {
+
+    RdList.get(function(data){
+        $scope.items = data;
+        $scope.displayeditem = data[99];
+    });
+    
+    $scope.show = function(item){
+        $scope.displayeditem = item;
+    };
+});
+
+
+
+app.controller('newsCtrl', function($scope, $http){
+
+    $http.get('../articles.json').success(function(callback){
+        $scope.articles = callback;
+    });
+
+});
