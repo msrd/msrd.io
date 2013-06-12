884b845cc28fc994ce066335caf3d261e5e33281
Jason Jarrett <jason@elegantcode.com>
2013-06-12 13:47:01 -0700

    cleanup package.json and grunt loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,9 @@ c = {
 };
 
 module.exports = function (grunt) {
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    // automatically load grunt plugins
+    require("matchdep").filterAll('grunt-*', require('./package.json')).forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
         watch: {
