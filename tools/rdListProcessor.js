2b149b83f6ae4b5cd73883d2e3a0fc9f95d4f6c5
Scott Stanfield <scott@vertigo.com>
2013-06-22 22:29:43 -0700

    Added RD list

--- tools/rdListProcessor.js
+++ tools/rdListProcessor.js
@@ -1,45 +1,65 @@
-"use strict";
+(function() {
+    'use strict';
 
-module.exports = function (fromFile, toFile, prettifyJson) {
-    var yaml = require('js-yaml');
-    var fs = require('fs');
-    var md5 = require('MD5');
-    var path = require("path");
+    module.exports = function (fromFile, toFile, prettifyJson) {
+        var yaml = require('js-yaml');
+        var fs = require('fs');
+        var md5 = require('MD5');
+        var path = require('path');
 
-    var i, j, rdlistCount, recordProcessorsCount;
+        var rdlistCount, recordProcessorsCount;
 
-    fromFile = path.resolve(fromFile);
-    toFile = path.resolve(toFile);
+        fromFile = path.resolve(fromFile);
+        toFile = path.resolve(toFile);
 
-    var yamlString = fs.readFileSync(fromFile, "utf8");
+        var yamlString = fs.readFileSync(fromFile, 'utf8');
 
-    var rdlist = yaml.load(yamlString, {
-        filename: fromFile
-    });
-
-    var applyGravatarHash = function(item) {
-
-        // Hash email with md5
-        // http://en.gravatar.com/site/implement/hash/
-        // http://en.gravatar.com/site/implement/images/
-
-        var email = item.Email;
-        if(email) {
-            item.GravatarHash = md5(email.trim().toLowerCase());
-        }
-        return item;
-    }
-
-    var recordProcessors = [
-        applyGravatarHash
-    ];
-
-    rdlist.forEach(function (item) {
-        recordProcessors.forEach(function (processor) {
-            processor(item);
+        var rdlist = yaml.load(yamlString, {
+            filename: fromFile
         });
-    });
 
-    fs.writeFileSync(toFile, JSON.stringify(rdlist, null, prettifyJson ? "\t" : ""));
-}
+        var applyGravatarHash = function(item) {
+
+            // Hash email with md5
+            // http://en.gravatar.com/site/implement/hash/
+            // http://en.gravatar.com/site/implement/images/
+
+            var email = item.Email;
+            if(email) {
+                item.GravatarHash = md5(email.trim().toLowerCase());
+            }
+            return item;
+        };
+
+        var concatPlaceName = function(item) {
+            if (item.State === null || item.State === '') {
+                item._placename = item.City + ', ' + item.Country;
+            } else {
+                item._placename = item.City + ', ' + item.State + ' ' + item.Country;
+            }
+
+            return item;
+        };
+
+        var gpsLookup = function(item) {
+            // hardcoded for now
+            item.Location = "41.355, -72.099";
+            return item;
+        };
+
+        var recordProcessors = [
+            applyGravatarHash, 
+            concatPlaceName,
+            gpsLookup
+        ];
+
+        rdlist.forEach(function (item) {
+            recordProcessors.forEach(function (processor) {
+                processor(item);
+            });
+        });
+
+        fs.writeFileSync(toFile, JSON.stringify(rdlist, null, prettifyJson ? "\t" : ""));
+    }
+})();
 
