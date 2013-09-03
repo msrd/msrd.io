6cc0455c6e9451d92a739a6939d25b127926e295
Scott Stanfield <scott@vertigo.com>
2013-09-03 06:52:29 -0700

    fixed up tab formatting

--- source/js/map.js
+++ source/js/map.js
@@ -1,74 +1,62 @@
 (function() {
-    "use strict";
+	"use strict";
 
-      function initialize() {
+	function initialize() {
 
-          var center = new google.maps.LatLng(26.130, 14.111)
-          var options = {
-              'zoom': 2,
-              'center': center,
-              'mapTypeId': google.maps.MapTypeId.ROADMAP
-          };
-          var map = new google.maps.Map(document.getElementById('map_canvas'), options);
-          var mcOptions = {
-              gridSize: 100,
-              maxZoom: 7
-          };
+		var center = new google.maps.LatLng(26.130, 14.111)
+		var options = {
+			'zoom': 2,
+			'center': center,
+			'mapTypeId': google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+		var mcOptions = {
+			gridSize: 100,
+			maxZoom: 7
+		};
 
-          function parseLocation(people) {
-              var i, peopleCount;
-              var markers = [];
-              var infoWindow = new google.maps.InfoWindow();
-              var latLongFunc = google.maps.LatLng;
-              var markerFunc = google.maps.Marker;
-              var marker;
-              for (i = 0, peopleCount = people.length; i < peopleCount; i++) {
-                  var person = people[i];
+		function parseLocation(people) {
+			var i, peopleCount;
+			var markers = [];
+			var infoWindow = new google.maps.InfoWindow();
+			var latLongFunc = google.maps.LatLng;
+			var markerFunc = google.maps.Marker;
+			var marker;
 
-                  // If there's no location, can't map: move to next person
-                  if (!person.hasOwnProperty('_location'))
-                      continue;
-                  var latLng = new latLongFunc(person._location.lat, person._location.lng);
+			for (i = 0, peopleCount = people.length; i < peopleCount; i++) {
+				var person = people[i];
 
-                  var last = person.Last;
-                  var first = person.First;
-                  var city = person.City;
-                  var state = person.State;
-                  var country = person.Country;
-                  var icon = person.Icon;
+				// If there's no location, can't map: move to next person
+				if (!person.hasOwnProperty('_location'))
+					continue;
 
-                    marker = new markerFunc({
-                     'position': latLng,
-                    });
+				var latLng = new latLongFunc(person._location.lat, person._location.lng);
+				marker = new markerFunc({
+					'position': latLng,
+				});
 
-                  // if(person.GravatarHash) {
-                  //   marker = new RichMarker({
-                  //       'position': latLng,
-                  //       'content': person.GravatarHash ? '<div class="circularIcon"><img style="border-radius: 50%;-moz-border-radius: 50%;-webkit-border-radius: 50%;" src="http://www.gravatar.com/avatar/' + person.GravatarHash + '"/></div>' : null,
-                  //       'anchor': RichMarkerPosition.MIDDLE,
-                  //       'flat':true
-                  //   });
-                  // } else {
-                  //   marker = new markerFunc({
-                  //       'position': latLng,
-                  //   });
-                 // }
+				var last = person.Last;
+				var first = person.First;
+				var city = person.City;
+				var state = person.State;
+				var country = person.Country;
+				var icon = person.Icon;
 
-                 markers.push(marker);
-                  var content = '<div class="info-window"><span class="director-name">' + first + ' ' + last + '</span><span class="director-location">' + city + ', ' + state + ' ' + country + '</span> <div class="circularIcon"><img style="border-radius: 50%;-moz-border-radius: 50%;-webkit-border-radius: 50%;" src="http://www.gravatar.com/avatar/' + person.GravatarHash + '"/></div>    </div>';
-                  bindInfoWindow(marker, map, infoWindow, content);
-              }
-              var mc = new MarkerClusterer(map, markers, mcOptions);
-          };
+				markers.push(marker);
+				var content = '<div class="info-window"><span class="director-name">' + first + ' ' + last + '</span><span class="director-location">' + city + ', ' + state + ' ' + country + '</span> <div class="circularIcon"><img style="border-radius: 50%;-moz-border-radius: 50%;-webkit-border-radius: 50%;" src="http://www.gravatar.com/avatar/' + person.GravatarHash + '"/></div>	</div>';
+				bindInfoWindow(marker, map, infoWindow, content);
+			}
+			var mc = new MarkerClusterer(map, markers, mcOptions);
+		};
 
-          $.getJSON("/rdlist.json", parseLocation);
+		$.getJSON("/rdlist.json", parseLocation);
 
-          function bindInfoWindow(marker, map, infoWindow, content) {
-              google.maps.event.addListener(marker, 'click', function () {
-                      infoWindow.setContent(content);
-                      infoWindow.open(map, marker);
-                  });
-          }
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+		function bindInfoWindow(marker, map, infoWindow, content) {
+			google.maps.event.addListener(marker, 'click', function () {
+					infoWindow.setContent(content);
+					infoWindow.open(map, marker);
+				});
+		}
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
 })();
