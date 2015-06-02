bee941c038b71b95997eaf9ea11e07f01c3c8b1f
Scott Stanfield <scott@vertigo.com>
2015-06-02 10:56:43 -0700

    jade doctype 5 was depricated

---
+++ source/js/gauges.js
@@ -0,0 +1,13 @@
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '50cf5fd6f5a1f51568000049');
+    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+    t.src = 'https://track.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+</script>
